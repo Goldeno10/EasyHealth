b8e2c285abad3f3a4035cc57590531f1fd786ab4
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-08-16 12:11:34 +0100

    fixed vercel error

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
+// const express = require("express");
+// const cors = require("cors");
+// require("dotenv").config();
+// require("./db/conn");
+// const userRouter = require("./routes/userRoutes");
+// const doctorRouter = require("./routes/doctorRoutes");
+// const appointRouter = require("./routes/appointRoutes");
+// const path = require("path");
+// const notificationRouter = require("./routes/notificationRouter");
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/user", userRouter);
+// app.use("/api/doctor", doctorRouter);
+// app.use("/api/appointment", appointRouter);
+// app.use("/api/notification", notificationRouter);
+// app.use(express.static(path.join(__dirname, "./client/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// app.listen(port, () => {});
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -5,7 +32,6 @@ require("./db/conn");
 const userRouter = require("./routes/userRoutes");
 const doctorRouter = require("./routes/doctorRoutes");
 const appointRouter = require("./routes/appointRoutes");
-const path = require("path");
 const notificationRouter = require("./routes/notificationRouter");
 
 const app = express();
@@ -17,10 +43,7 @@ app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
-app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-app.listen(port, () => {});
